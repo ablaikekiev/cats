5e4cfa013528f8777084c52fe1b2ed5f9116ff48
Ablai <ablaa.00@gmail.com>
2021-05-28 19:52:31 +0600

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,40 @@
+window.onload = function () {
+    const burgerMenu = document.querySelector('.burger-menu');
+    const header = document.querySelector('.header');
+
+    burgerMenu.addEventListener('click', () => {
+        burgerMenu.classList.toggle('active');
+        header.classList.toggle('active');
+    });
+
+    const prices = Array.from(document.getElementsByClassName('main-flexBox__card'))
+        .map((it => it.children))
+        .map(it => it[3].innerHTML).sort();
+    console.log(prices);
+
+    const btn = Array.from(document.querySelectorAll('.main-flexBox__btn'));
+        btn.map(it => it.addEventListener('click', () => {
+
+            it.classList.add('active');
+            it.textContent = 'Продан'
+
+        }))
+    const svg = Array.from(document.querySelectorAll('path'));
+        svg.map(it => it.addEventListener('click', () => {
+            it.setAttribute('fill', 'red')
+        }))
+
+    $(window).scroll(function () {
+        if($(this).scrollTop() > 800){
+            $('.scroll-up').fadeIn();
+        } else {
+            $('.scroll-up').fadeOut();
+        }
+    })
+};
+
+
+
+
+
+
